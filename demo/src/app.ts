802b55d2df8dd0b37f0a21a2f3a1bbd1d23b41b7
Hieu Nguyen <hieu.nguyen@banvien.com.vn>
2023-11-27 11:13:10 +0700

    Checking for GDB Worker

--- demo/src/app.ts
+++ demo/src/app.ts
@@ -2,43 +2,6 @@ import { Terminal } from "xterm";
 import { openpty } from "xterm-pty";
 import "xterm/css/xterm.css";
 
-// vim
-(async() => {
-  const vimXterm = new Terminal();
-  const vimDiv = document.getElementById("vim-xterm");
-  if (vimDiv) vimXterm.open(vimDiv);
-
-  const { master, slave } = openpty();
-  vimXterm.loadAddon(master);
-
-  const { default: initEmscripten } = await import("../static/vim-core.js");
-  const module = await initEmscripten({
-    pty: slave,
-  });
-
-  const links: { [key: string]: HTMLAnchorElement } = {};
-
-  module.FS.trackingDelegate["onWriteToFile"] = (path: string) => {
-    if (!path.startsWith("/home/web_user/") || path.endsWith(".swp")) {
-      return;
-    }
-    const data = module.FS.readFile(path);
-    const blob = new Blob([data], { type: "text/plain" });
-    const url = window.URL.createObjectURL(blob);
-    if (path in links) {
-      links[path].href = url;
-    } else {
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = path.split("/").pop() || "dummy.txt";
-      a.innerText = path;
-      links[path] = a;
-      const files = document.getElementById("files");
-      files?.appendChild(a);
-    }
-  };
-})();
-
 const entry = (id: string, loadJS: () => Promise<any>) => {
   const xterm = new Terminal();
   const div = document.getElementById(id + "-xterm");
@@ -56,9 +19,8 @@ const entry = (id: string, loadJS: () => Promise<any>) => {
       onExit: () => {
         module = undefined;
         button.disabled = false;
-        xterm.clear();
         xterm.write("\r[Terminated. Push the 'Run' button to restart it.]\r\n");
-      },
+      }
     });
   };
   button.onclick = invoke;
@@ -66,8 +28,5 @@ const entry = (id: string, loadJS: () => Promise<any>) => {
 
 // As "new Worker(...)" is an idiom to allow parcel to track dependency,
 // we need to write them literally
-entry("example", () => import("../static/example-core.js"));
 
-entry("sl", () => import("../static/sl-core.js"));
-
-entry("sloane", () => import("../static/sloane-core.js"));
+entry("gdb", () => import("../static/gdb.js"));
